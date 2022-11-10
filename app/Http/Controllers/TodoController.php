8bb7074914098dd36761e43cef6f711bc98747e0
Mark Schumacher <schuma1022@gmail.com>
2022-11-09 19:43:23 -0600

    create todo controller

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    public function index () {
+        // Response function allows us to send response
+        return response(Todo::all(), 200)
+        ->header('Content-Type', 'application/json');
+    }
+}
